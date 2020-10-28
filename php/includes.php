900e687b4200c828c3de45dc1a8cd04e30a979fe
Mr-MSA <72319450+Mr-MSA@users.noreply.github.com>
2020-10-28 23:26:51 +0330

    Add files via upload

---
+++ php/includes.php
@@ -0,0 +1,7 @@
+<?php
+
+include "File/body.htm" ;
+include "File/var.php" ;
+include "File/check.php" ;
+
+?>
--- php/index.php
+++ php/index.php
@@ -1,21 +1,5 @@
 <?php
 
- include 'File/includes.php';
-
- if ($emptycode == 'false') {
-   echo $Div_one ;
-   echo $Name_Hash[0] ;
-   echo $Yor_Text ; 
-   
- 	for ($o=0; $o<$avc;$o++) { 
-		echo '<br><br>'. $hash_name[$o] .'<br>'. htmlspecialchars($hash[$o]) ;
-	}
-   
-   echo $Name_Hash[1] ;
-   echo $End_Tag ;
-   echo $Name_Hash[2] ;
- }else {
- 	 	 include "File/footer.htm" ;
- }
-
+header('location:../index.php');
+exit ; 
 ?>
